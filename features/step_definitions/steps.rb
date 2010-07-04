9605edb01fb018213ddd2ce1885151504dd3fe44
Kerry Buckley <kerryjbuckley@gmail.com>
2010-07-04 09:51:06 +0100

    Add pending step definitions

---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,11 @@
+Given %r/^we start a new game with (\d+) players$/ do |number_of_players|
+  pending # express the regexp above with the code you wish you had
+end
+
+When %r/^player (\d+) throws (.*)$/ do |throws|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then %r/^player (\d+)'s score is (\d+)$/ do |score|
+  pending # express the regexp above with the code you wish you had
+end
